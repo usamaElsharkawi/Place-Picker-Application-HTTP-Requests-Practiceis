a67e87fd7ba4b6081bcc3dff3861f13609e74d69
Usama El-Sharkawi <usamasharkawi@gmail.com>
2025-12-29 21:42:10 +0200

    transforming fetched data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import ErrorPage from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isfetching, setIsFetching] = useState(false);
@@ -17,13 +18,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Faild to fetch places");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false); 
+        });
+
       } catch (error) {
         setError({
           message: error.message || "Could not fetch places,please try later",
         });
+        setIsFetching(false);
       }
-      setIsFetching(false);
+       
     }
     fetchPlaces();
   }, []);
